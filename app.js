ceac28c4461f64be5a91f403e7321dc6d2f78051
archeelp <archeelparekh11@gmail.com>
2021-04-04 20:21:01 +0530

    cors configured

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express"),
+    cors = require("cors"),
     app = express();
 
+app.use(cors({origin:['http://localhost:3000']}));
 app.use(express.static(__dirname + "/public"));
+app.set('views', __dirname + '/views');
+
+app.get('/',function(req,res) {
+    res.sendFile('views/index.html', {root: __dirname })
+});
 
 app.listen(process.env.port || 3000,() => {
     console.log("server is running");
