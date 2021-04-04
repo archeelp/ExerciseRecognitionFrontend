48d2913cfbc0e9b6040cf1d74846cc7ce1ad7b0c
archeelp <archeelparekh11@gmail.com>
2021-04-04 20:31:03 +0530

    start script

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/',function(req,res) {
     res.sendFile('views/index.html', {root: __dirname })
 });
 
-app.listen(process.env.Port || 3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log("server is running");
 })
